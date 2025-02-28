558c68c8714b025adde1689cc47086c27a5ef249
Kimhyochan <hyochan0220@gmail.com>
2025-02-28 23:09:50 +0900

    백준 1149번 DP로 풀이이

--- Codingtest/B_1149.py
+++ Codingtest/B_1149.py
@@ -1,31 +1,15 @@
 import sys
 
-def find_index_2d(array, target):
-    for i in range(len(array)):
-        for j in range(len(array[i])):
-            if array[i][j] == target:
-                return (j)
-    return None
-
 input=sys.stdin.readline
-expence=[]
-DP=[]
-anwser=0
+n = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)]
 
-N=int(input())
-for i in range(N):
-    expence.append(list(map(int,input().split())))
+dp = [[0]*3 for _ in range(n)]
+dp[0] = arr[0]
 
-for i in range(N):
-    if i==0:
-        anwser+=min(expence[i])
-        DP.append(find_index_2d(expence,min(expence[i])))
-    else:
-        if find_index_2d(expence,min(expence[i]))==DP[i-1]:
-            expence[i][find_index_2d(expence,min(expence[i]))]+=1000
-            anwser+=min(expence[i])
-            DP.append(find_index_2d(expence,min(expence[i])))
-        else:
-            anwser+=min(expence[i])
-            DP.append(find_index_2d(expence,min(expence[i])))
-print(anwser)
+for i in range(1,n):
+    dp[i][0] = min(dp[i-1][1], dp[i-1][2]) + arr[i][0]
+    dp[i][1] = min(dp[i-1][0], dp[i-1][2]) + arr[i][1]
+    dp[i][2] = min(dp[i-1][0], dp[i-1][1]) + arr[i][2]
+
+print(min(dp[n-1]))
