e88ec15f5c86df3667cb9a848eb728413395d680
Kimhyochan <hyochan0220@gmail.com>
2025-03-22 23:42:07 +0900

    2805번 풀이 완탐

--- Codingtest/B_2805.py
+++ Codingtest/B_2805.py
@@ -1,10 +1,15 @@
-import sys
+N, M = map(int, input().split())
+tree = list(map(int, input().split()))
+start, end = 1, max(tree)
 
-input=sys.stdin.readline
-DP1=[]
-DP2=[]
-N,M=map(int,input().split())
-Tree=list(map(int,input().split()))
-Tree.sort(reverse=True)
-while True:
-    pass
+while start <= end:
+    mid = (start+end) // 2
+    log = 0
+    for i in tree:
+        if i >= mid:
+            log += i - mid
+    if log >= M:
+        start = mid + 1
+    else:
+        end = mid - 1
+print(end)
