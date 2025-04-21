1e34eeee3ac2a7e221432b9934c8642877bf1c4b
Kimhyochan <hyochan0220@gmail.com>
2025-04-21 23:09:20 +0900

    백준 1522 풀이이

---
+++ Codingtest/B_1522.py
@@ -0,0 +1,7 @@
+String=input()
+Counta=String.count('a')
+String+=String[0:Counta-1]
+min_val = float('inf')
+for i in range(len(String)-(Counta-1)):
+    min_val = min(min_val,String[i:i+Counta].count('b'))
+print(min_val)
